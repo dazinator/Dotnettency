ce32a13f2a6383f9c4d987fc321c1be9409c689a
Darrell Tunnell <darrell.tunnell@covarius.com>
2019-05-03 17:11:01 +0100

    Added selfhost

---
+++ src/Sample.Owin.SelfHost/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Owin.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (WebApp.Start<Startup>("http://localhost:12345"))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+#if DEBUG
+            app.UseErrorPage();
+#endif
+            app.UseWelcomePage("/");
+        }
+    }
+}
