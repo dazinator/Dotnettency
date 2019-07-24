ac5129cd782dd77c2cb13686d02c162c6a15e070
Darrell Tunnell <darrell.tunnell@covarius.com>
2019-07-24 17:36:16 +0100

    #52 - EFCore multitenant identity dbcontext base types

--- src/Dotnettency.EFCore.Identity/MultitenantIdentityDbContext.cs
+++ src/Dotnettency.EFCore.Identity/MultitenantIdentityDbContext.cs
@@ -9,35 +9,51 @@ using System.Threading.Tasks;
 
 namespace Dotnettency.EFCore.Identity
 {
-    public class MultitenantIdentityDbContext<TDbContext, TTenant, TIdType, TUser, TRole, TKey> : IdentityDbContext<TUser, TRole, TKey>
+    public abstract class MultitenantIdentityDbContext<TDbContext, TTenant, TIdType, TUser, TRole, TKey> : MultitenantIdentityDbContext<TDbContext, TTenant, TIdType, TUser, TRole, TKey, IdentityUserClaim<TKey>, IdentityUserRole<TKey>, IdentityUserLogin<TKey>, IdentityRoleClaim<TKey>, IdentityUserToken<TKey>>
         where TUser : IdentityUser<TKey>
         where TRole : IdentityRole<TKey>
         where TKey : IEquatable<TKey>
+    {     
+
+        public MultitenantIdentityDbContext(DbContextOptions<MultitenantIdentityDbContext<TDbContext, TTenant, TIdType, TUser, TRole, TKey>> options, Task<TTenant> tenant)
+       : base(options, tenant)
+        {
+           
+        }
+
+    }
+
+    public abstract class MultitenantIdentityDbContext<TDbContext, TTenant, TTenantIdType, TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken> : IdentityDbContext<TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken>
+    where TUser : IdentityUser<TKey>
+    where TRole : IdentityRole<TKey>
+    where TKey : IEquatable<TKey>
+    where TUserClaim : IdentityUserClaim<TKey>
+    where TUserRole : IdentityUserRole<TKey>
+    where TUserLogin : IdentityUserLogin<TKey>
+    where TRoleClaim : IdentityRoleClaim<TKey>
+    where TUserToken : IdentityUserToken<TKey>
     {
 
         private readonly Task<TTenant> _tenant;
-        private Lazy<TIdType> _tenantId;
+        private Lazy<TTenantIdType> _tenantId;
 
         private static List<Action<DbContext>> _setTenantIdOnSaveCallbacks = new List<Action<DbContext>>();
 
 
-        public MultitenantIdentityDbContext(DbContextOptions<MultitenantIdentityDbContext<TDbContext, TTenant, TIdType, TUser, TRole, TKey>> options, Task<TTenant> tenant)
+        public MultitenantIdentityDbContext(DbContextOptions options, Task<TTenant> tenant)
        : base(options)
         {
             _tenant = tenant;
-            _tenantId = new Lazy<TIdType>(() =>
+            _tenantId = new Lazy<TTenantIdType>(() =>
             {
                 var t = _tenant.Result;
                 return GetTenantId(t);
             });
         }
 
-        protected virtual TIdType GetTenantId(TTenant tenant)
-        {
-            return default(TIdType);
-        }
+        protected abstract TTenantIdType GetTenantId(TTenant tenant);       
 
-        private TIdType TenantId
+        private TTenantIdType TenantId
         {
             get
             {
@@ -45,15 +61,15 @@ namespace Dotnettency.EFCore.Identity
             }
         }
 
-        protected void HasTenantIdFilter<T>(ModelBuilder modelBuilder, string tenantIdPropertyName, Expression<Func<T, TIdType>> idExpression)
+        protected void HasTenantIdFilter<T>(ModelBuilder modelBuilder, string tenantIdPropertyName, Expression<Func<T, TTenantIdType>> idExpression)
   where T : class
         {
 
-            modelBuilder.Entity<T>().Property<TIdType>(tenantIdPropertyName);
+            modelBuilder.Entity<T>().Property<TTenantIdType>(tenantIdPropertyName);
 
             var newExp = Expression.Lambda<Func<T, bool>>(
                     Expression.Equal(idExpression.Body, Expression.Property(Expression.Constant(this),
-                    typeof(MultitenantIdentityDbContext<TDbContext, TTenant, TIdType, TUser, TRole, TKey>),
+                    typeof(MultitenantIdentityDbContext<TDbContext, TTenant, TTenantIdType, TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken>),
                     nameof(TenantId))), idExpression.Parameters);
 
             modelBuilder.Entity<T>().HasQueryFilter(newExp);
@@ -90,7 +106,7 @@ namespace Dotnettency.EFCore.Identity
             return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
         }
 
-        private static void SetTenantIdProperty<TEntity>(string propertyName, TIdType id, DbContext db)
+        private static void SetTenantIdProperty<TEntity>(string propertyName, TTenantIdType id, DbContext db)
             where TEntity : class
         {
             foreach (var item in db.ChangeTracker.Entries<TEntity>())
@@ -113,4 +129,5 @@ namespace Dotnettency.EFCore.Identity
 
 
     }
+
 }
