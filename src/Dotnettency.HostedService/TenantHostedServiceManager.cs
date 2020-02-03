759882f3c279430c63b74911148b518ca08bf1c1
Darrell Tunnell <darrell.tunnell@googlemail.com>
2020-02-03 14:48:17 +0000

    Tidying

--- src/Dotnettency.HostedService/TenantHostedServiceManager.cs
+++ src/Dotnettency.HostedService/TenantHostedServiceManager.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Dotnettency
 {
-    public class TenantHostedServiceManager<TTenant>
+    public class TenantHostedServiceManager<TTenant> : IDisposable
          where TTenant : class
     {
         // private readonly TenantShellItemBuilderContext<TTenant> _context;
@@ -96,6 +96,51 @@ namespace Dotnettency
 
             _logger.LogInformation("Tenant hosted services were stopped.");
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    try
+                    {
+                        StopAsync(default(CancellationToken)).ConfigureAwait(false).GetAwaiter().GetResult();
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogError(e, "error on dispose");
+                        throw;
+                    }
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~TenantHostedServiceManager()
+        // {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
 
 
 
